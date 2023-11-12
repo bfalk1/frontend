f6835bb10e682b7a3c554d4ce1b3c8c9dca6f55b
Benjamin Falkner <bfalkner9@gmail.com>
2023-11-12 13:11:31 -0500

    idk

--- FrontEnd/Components/eventCard/eventCardTemplate.js
+++ FrontEnd/Components/eventCard/eventCardTemplate.js
@@ -3,52 +3,53 @@ import { InputImage } from '../input-image/input-image';
 import {CountdownTimer} from '../countdown-timer/countdown-timer';
 
 export const eventCardTemplate = (context) => {
-  return html`<style>
-    .course-card {
-      width: 250px; /* Adjust as needed */
-      border: 1px solid #ddd;
-      box-shadow: 0 0px 6px rgba(0, 0, 0, 0.4);
-      transition: 0.3s;
-      width: 300px;
-      border-radius: 8px;
-      overflow: hidden; /* This will make sure that the image and content stay within the card boundaries */
-      margin: 10px;
-      position: relative; /* Positioning context for the absolute positioned icons later */
-  }
-
-    .course-card img {
+  return html`
+    <style>
+      .card {
+        box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.4);
+        transition: 0.3s;
+        border-radius: 10px;
         width: 100%;
-        height: auto;
-        display: block; /* Removes any potential space at the bottom of the image */
-    }
-
-    .course-info {
-        padding: 10px;
-    }
-
-    .course-card h4 {
-        margin: 0;
-        padding-bottom: 5px;
-        color: rgb(6, 28, 113);
-        font-size: 16px; /* Adjust as needed */
-    }
-    .course-card p {
-      margin: 0;
-      color: #777; /* Slightly grayed out text for the date, adjust as needed */
-    }
-      .description {
-        padding: 5px;
-        margin-top: 10px;
-        font-size: 14px;
-        color: #777;
+        max-width: 00px;
+        background-color: white;
+        overflow: hidden;
+        margin-bottom: 3px;
       }
+      
+
+      .card-img {
+        border: none;
+        object-fit: cover;
+        resize: none;
+        width: 100%; /* Ensure the image takes up 100% of the container width */
+        height: 100%; /* Ensure the image takes up 100% of the container height */
+        border-radius: 10px; /* Match the border-radius of the .card class */
+      }
+
+
+      .title {
+        text-align: left;
+        margin: 0;
+        color: black;
+        font-size: 1.4rem; /* Increased header font size */
+        padding-left:3%;
+        padding-top:3%;
+      }
+
+      .description {
+        font-size: 0.9rem;
+        text-align: left;
+        color: black;
+        padding: 3%;
+      }
+      
     </style>
-    <div class="course-card">
-    <input-image .selectedFile=${context.img}></input-image>
-    <div class="course-info">
-        <h4>${context.title}</h4>
-        <p class="description">${context.description}</p>
-        <countdown-timer></countdown-timer>
-    </div>
-  </div>`;
+
+    <div class="card">
+      <h3 class="title">${context.title}</h3>
+      <p class="description">${context.description}</p>
+      <countdown-timer></countdown-timer>
+      <input-image class="card-img" .selectedFile=${context.img}></input-image>
+      
+    </div>`;
 };
